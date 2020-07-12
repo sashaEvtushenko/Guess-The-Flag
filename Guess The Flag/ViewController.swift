c7b14109d6ee8c7624649a1b2f14f4d2436276a3
Sasha Evtushenko <sasha.evtushenko.md@gmail.com>
2020-07-12 20:21:22 +0300

    Initial Commit

---
+++ Guess The Flag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Guess The Flag
+//
+//  Created by Sasha Evtushenko on 7/12/20.
+//  Copyright © 2020 Sasha Evtushenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
